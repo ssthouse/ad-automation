29f016caefd095fee6045e48ef7154f5c815394b
shen_s <shen_s@worksap.co.jp>
2018-06-05 18:38:32 +0800

    Initial commit

---
+++ backend/src/main/java/com/ssthouse/adautomation/AdAutomationApplication.java
@@ -0,0 +1,12 @@
+package com.ssthouse.adautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdAutomationApplication.class, args);
+    }
+}
