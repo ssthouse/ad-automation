1a4bc3901fb3371d1c88e919685373e5fc032ae1
ssthouse <ssthouse@163.com>
2018-06-06 22:03:05 +0800

    Test add frontend proxy for dev

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -10,7 +10,6 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -40,7 +39,17 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
+    proxyTable: {
+      // proxy all requests starting with /api to jsonplaceholder
+      '/automation': {
+        target: 'http://localhost:8080', // <-- Spring app running here
+        changeOrigin: true,
+        pathRewrite: {
+          '^/automation': ''
+        }
+      }
+    }
   },
 
   build: {
