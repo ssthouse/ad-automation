0e886bed2ded46227c4e084ca9b1af2657eacdfa
ssthouse <ssthouse@163.com>
2018-06-11 12:19:24 +0800

    Add simple layout for dashboard page

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="dashboard">
-    Dashboard
-    <el-button @click="test"></el-button>
+    <el-container id="dashboard-container">
+
+      <el-header>Header</el-header>
+
+      <el-container>
+        <el-aside width="200px">
+          <side-menu id="side-menu"></side-menu>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import SideMenu from '../components/SideMenu'
+
 export default {
   name: 'dashboard',
-  components: {},
+  components: { 'side-menu': SideMenu },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -43,4 +56,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+#dashboard-container {
+  width: 100%;
+  height: 100%;
+}
 </style>
